a834586e57e80a8e901147ad93733d60418af126
GitHub Action <action@github.com>
2023-01-05 15:17:47 +0000

    Template cleanup

--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -61,7 +61,7 @@ publishing {
                     version = "$version-SNAPSHOT"
                 }
                 description.set("A template for Kotlin Android projects")
-                url.set("https://github.com/cortinico/kotlin-android-template/")
+                url.set("https://github.com/apmon2/androidtemplateapp/")
 
                 licenses {
                     license {
@@ -71,18 +71,18 @@ publishing {
                 }
                 developers {
                     developer {
-                        id.set("cortinico")
-                        name.set("Nicola Corti")
+                        id.set("apmon2")
+                        name.set("apmon2")
                     }
                 }
                 scm {
-                    connection.set("scm:git:git://github.com/cortinico/kotlin-android-template.git")
-                    developerConnection.set("scm:git:ssh://github.com/cortinico/kotlin-android-template.git")
-                    url.set("https://github.com/cortinico/kotlin-android-template/")
+                    connection.set("scm:git:git://github.com/apmon2/androidtemplateapp.git")
+                    developerConnection.set("scm:git:ssh://github.com/apmon2/androidtemplateapp.git")
+                    url.set("https://github.com/apmon2/androidtemplateapp/")
                 }
                 issueManagement {
                     system.set("GitHub Issues")
-                    url.set("https://github.com/cortinico/kotlin-android-template/issues")
+                    url.set("https://github.com/apmon2/androidtemplateapp/issues")
                 }
             }
         }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = ("kotlin-android-template")
+rootProject.name = ("androidtemplateapp")
 
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
